1e3b9bca4fff629734473ee1278ccfe620525a4d
Carlos Pereira <carloscoto0103@gmail.com>
2021-12-30 19:27:08 -0600

    DashboardRotes added

--- src/components/routers/AppRoute.js
+++ src/components/routers/AppRoute.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter  } from "react-router-dom";
-import { DcScreen } from '../dc/DcScreen';
+
 import { LoginScreen } from '../login/LoginScreen';
-import { MarvelScreen } from '../marvel/MarvelScreen';
-import { SearchScreen } from '../search/SearchScreen';
-import { Navbar } from '../ui/Navbar';
+
+import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRoute = () => {
     return (
         <BrowserRouter >
             
-            <Navbar />
-
             <Routes>
-                <Route path="/"       element={<MarvelScreen />} />
-                <Route path="/marvel" element={<MarvelScreen />} />
-                <Route path="/dc"     element={<DcScreen />} />
-                <Route path="/search" element={<SearchScreen />} />
+        
                 <Route path="/login"  element={<LoginScreen />} />
+
+                <Route path="/*" element={<DashboardRoutes />} />
+
             </Routes>
         </BrowserRouter >
     )
---
+++ src/components/routers/DashboardRoutes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route  } from "react-router-dom";
+
+import { Navbar } from "../ui/Navbar";
+
+import { MarvelScreen } from '../marvel/MarvelScreen';
+import { SearchScreen } from '../search/SearchScreen';
+import { DcScreen } from '../dc/DcScreen';
+import { HeroScreen } from '../hero/HeroScreen';
+
+export const DashboardRoutes = () => {
+    return (
+        <>
+            <Navbar />
+
+            <Routes>
+                <Route path="marvel" element={<MarvelScreen />} />
+                <Route path="dc"     element={<DcScreen />} />
+
+                <Route path="search" element={<SearchScreen />} />
+                <Route path="hero"   element={<HeroScreen />} />
+
+                <Route path="/"      element={<MarvelScreen />} />
+            </Routes>
+        </>
+    )
+}
