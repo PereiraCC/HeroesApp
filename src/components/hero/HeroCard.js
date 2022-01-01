a5f26f5f607e1dca1250a764e70d95d40f0681c7
Carlos Pereira <carloscoto0103@gmail.com>
2021-12-31 19:55:31 -0600

    HeroCard added

---
+++ src/components/hero/HeroCard.js
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom';
+
+export const HeroCard = ( { 
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters ,
+} ) => {
+
+    const imagePath = `/assets/${id}.jpg`;
+
+    return (
+
+        <div className='col'>
+            <div className='card'>
+                
+                <div className='row no-gutters'>
+                    <div className='col-md-4'>
+                        <img src={ imagePath } alt={superhero} className='card-img'/>
+                    </div>
+                    <div className='col-8'>
+
+                        <div className='card-body'>
+
+                            <h5 className='card-title'> { superhero } </h5>
+                            <h5 className='card-text'> { alter_ego } </h5>
+
+                            {
+                                ( alter_ego !== characters ) 
+                                    && <p className='text-muted'>{ characters }</p>
+                            }
+
+                            <p className='card-text'>
+                                <small className='tex-muted'>{ first_appearance }</small>
+                            </p>
+
+                            <Link to={`/hero/${id}`}>
+                                More...
+                            </Link>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+    )
+}
