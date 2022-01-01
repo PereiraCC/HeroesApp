227113c9213f81c5079f125cf715e77189c10423
Carlos Pereira <carloscoto0103@gmail.com>
2021-12-31 20:12:04 -0600

    getHeroById and HeroScreen created

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import { useParams, Navigate } from 'react-router-dom';
+
+import { getHeroesById } from '../../selectors/getHeroById';
+
 
 export const HeroScreen = () => {
+
+    const { heroId } = useParams();
+
+    const hero = getHeroesById( heroId );
+
+    if( !hero ) {
+        return <Navigate to='/' />
+    }
+
     return (
         <div>
             <h1>HeroScreen</h1>
+            <p>
+                {hero.superhero}
+            </p>
         </div>
     )
 }
