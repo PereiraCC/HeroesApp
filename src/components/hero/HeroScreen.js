5c27f9f56cfee46e95942000dd85ded128e1aab5
Carlos Pereira <carloscoto0103@gmail.com>
2022-01-02 12:28:22 -0600

    HeroScreen style added

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,24 +1,66 @@
-import { useParams, Navigate } from 'react-router-dom';
+import { useParams, Navigate, useNavigate } from 'react-router-dom';
 
 import { getHeroesById } from '../../selectors/getHeroById';
 
 
 export const HeroScreen = () => {
 
+    const navigate = useNavigate();
+
     const { heroId } = useParams();
 
     const hero = getHeroesById( heroId );
+
+    const {
+        id,
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters
+    } = hero;
+    
+
+    const handleReturn = () => {
+        navigate(`/${publisher.split(' ')[0].toLowerCase()}`);
+        // navigate(-1);
+    }
 
     if( !hero ) {
         return <Navigate to='/' />
     }
 
+    const imagePath = `/assets/${id}.jpg`;
+
     return (
-        <div>
-            <h1>HeroScreen</h1>
-            <p>
-                {hero.superhero}
-            </p>
+        <div className='row mt-5'>
+            <div className='col-4' >
+                <img  
+                    src={ imagePath } 
+                    alt={ superhero }
+                    className='img-thumbnail'
+                />
+            </div>
+
+            <div className='col-8'>
+                <h3>{ superhero }</h3>
+                <ul className='list-group list-group-flush'>
+                    <li className='list-group-item'> <b>Alter ego:</b> { alter_ego } </li>
+                    <li className='list-group-item'> <b>Publisher:</b> { publisher } </li>
+                    <li className='list-group-item'> <b>First Appearance:</b> { first_appearance } </li>
+                </ul>
+
+                <h5 className='mt-3'>Characters</h5>
+                <p> { characters } </p>
+
+                <button 
+                    className='btn btn-outline-info'
+                    onClick={ handleReturn }
+                >
+                    Back
+                </button>
+
+            </div>
         </div>
     )
 }
