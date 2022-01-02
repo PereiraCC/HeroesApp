80ae25dcd6914ff5c3b37b51e39eb8e08c6033f8
Carlos Pereira <carloscoto0103@gmail.com>
2022-01-02 16:21:55 -0600

    useForm added and SearchScreen updated

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,52 @@
 import React from 'react';
 
+import { useForm } from '../../hook/useForm';
+
 export const SearchScreen = () => {
+
+
+    const [ { searchText }, handleInputChange, reset ] = useForm( {
+        searchText : ''
+    });
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log(searchText);
+        reset();
+    }
+
     return (
-        <div>
-            <h1>SearchScreen</h1>
-        </div>
+        <>
+            <h1>Searches</h1>
+            <hr />
+
+            <div className='row'>
+
+                <div className='col-5'>
+                    <h4>Search</h4>
+                    <hr />
+
+                    <form onSubmit={ handleSearch }>
+                        <input 
+                            type="text"
+                            placeholder='Search a hero'
+                            className='form-control'
+                            name='searchText'
+                            autoComplete='off'    
+                            onChange={ handleInputChange } 
+                            value={ searchText }
+                        />
+
+                        <button 
+                            className='btn btn-outline-primary mt-1 btn-block'
+                            type='submit'
+                        >
+                            Search...
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+        </>
     )
 }
