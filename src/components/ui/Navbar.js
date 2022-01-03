d3924dbd4452d29facfd8b46c686b6fb341d103b
Carlos Pereira <carloscoto0103@gmail.com>
2022-01-03 12:05:22 -0600

    logout added

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,13 +1,18 @@
 import React, { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+
+import { types } from '../../types/types';
 import { AuthContext } from '../../auth/authContext';
 
 export const Navbar = () => {
     
-    const { user } = useContext(AuthContext);
+    const { user, dispatch } = useContext(AuthContext);
     const navigate = useNavigate();
     
     const handleLogout = () => {
+
+        dispatch({ type : types.logout });
+
         navigate('/login', {
             replace: true
         });
