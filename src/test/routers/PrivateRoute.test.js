905182c5ede48a5412b963d6334b6ed1f0d998af
Carlos Pereira <carloscoto0103@gmail.com>
2022-01-19 17:20:41 -0600

    PrivateRoute tests added

---
+++ src/test/routers/PrivateRoute.test.js
@@ -0,0 +1,64 @@
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+import { AuthContext } from '../../auth/authContext';
+import { PrivateRoute } from '../../routers/PrivateRoute';
+
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    Navigate: () => <span>Getting out of here</span>
+}));
+
+describe('<PrivateRoute /> tests', () => {
+  
+    Storage.prototype.setItem = jest.fn();
+
+    test('should show the component if is authenticated and save in localStorage', () => {
+      
+        const contextValue = {
+            user: {
+                logged: true,
+                name : 'Pepe'
+            }
+        };
+
+        const wrapper = mount( 
+            <AuthContext.Provider value={ contextValue }>
+                <MemoryRouter initialEntries={['/']}>
+                    <PrivateRoute>
+                        <h1>Private component</h1>
+                    </PrivateRoute>
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        expect( wrapper.text().trim() ).toBe( 'Private component' );
+        expect( localStorage.setItem ).toHaveBeenCalledWith('lastPath', '/');
+
+    });
+
+    test('should block the component if is not authenticated', () => {
+      
+        const contextValue = {
+            user: {
+                logged: false,
+            }
+        };
+
+        const wrapper = mount( 
+            <AuthContext.Provider value={ contextValue }>
+                <MemoryRouter initialEntries={['/']}>
+                    <PrivateRoute>
+                        <h1>Private component</h1>
+                    </PrivateRoute>
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        expect( wrapper.text().trim() ).toBe( 'Getting out of here' );
+
+    });
+    
+    
+
+});
